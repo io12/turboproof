1a277a12592e3d94f61268bb387c96a82a9a9f3e
Benjamin Levy <blevy@protonmail.com>
2019-06-19 19:10:01 -0400

    get input file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#[macro_use]
+extern crate clap;
+extern crate failure;
+extern crate lexpr;
+
+use std::path::{Path, PathBuf};
+
+use failure::Fallible;
+
+fn parse_args() -> clap::ArgMatches<'static> {
+    app_from_crate!()
+        .args_from_usage("<INPUT> 'The input file to evaluate'")
+        .get_matches()
+}
+
+fn get_input_file() -> PathBuf {
+    let args = parse_args();
+    let path = args.value_of_os("INPUT").expect("INPUT unspecified");
+    Path::new(path).to_path_buf()
+}
+
+fn try_main() -> Fallible<()> {
+    let inp = get_input_file();
+    println!("Got input file: {:?}", inp);
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = try_main() {
+        eprintln!("{}: error: {}", env!("CARGO_PKG_NAME"), err);
+        std::process::exit(1);
+    }
 }
