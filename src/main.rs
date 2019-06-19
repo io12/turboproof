d23a98b60ce8a86da42bc8d2ce9662990deedaf8
Benjamin Levy <blevy@protonmail.com>
2019-06-19 19:11:59 -0400

    add comments

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ use std::path::{Path, PathBuf};
 
 use failure::Fallible;
 
+// Use clap crate to parse args
 fn parse_args() -> clap::ArgMatches<'static> {
     app_from_crate!()
         .args_from_usage("<INPUT> 'The input file to evaluate'")
         .get_matches()
 }
 
+// Get the input file to evaluate by parsing the program args
 fn get_input_file() -> PathBuf {
     let args = parse_args();
     let path = args.value_of_os("INPUT").expect("INPUT unspecified");
@@ -26,6 +28,7 @@ fn try_main() -> Fallible<()> {
     Ok(())
 }
 
+// Wrapper of `try_main()` to handle propagated errors
 fn main() {
     if let Err(err) = try_main() {
         eprintln!("{}: error: {}", env!("CARGO_PKG_NAME"), err);
