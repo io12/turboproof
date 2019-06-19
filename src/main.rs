b67c8651d08b144051b45287675de8bfae8ff5a8
Benjamin Levy <blevy@protonmail.com>
2019-06-19 19:21:03 -0400

    parse file

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate clap;
 extern crate failure;
 extern crate lexpr;
 
+use std::fs::File;
 use std::path::{Path, PathBuf};
 
 use failure::Fallible;
@@ -14,16 +15,19 @@ fn parse_args() -> clap::ArgMatches<'static> {
         .get_matches()
 }
 
-// Get the input file to evaluate by parsing the program args
-fn get_input_file() -> PathBuf {
+// Get path of the input file to evaluate by parsing the program args
+fn get_input_path() -> PathBuf {
     let args = parse_args();
     let path = args.value_of_os("INPUT").expect("INPUT unspecified");
     Path::new(path).to_path_buf()
 }
 
 fn try_main() -> Fallible<()> {
-    let inp = get_input_file();
-    println!("Got input file: {:?}", inp);
+    let path = get_input_path();
+    let file = File::open(path)?;
+    let prog = lexpr::from_reader(file)?;
+
+    println!("{:?}", prog);
 
     Ok(())
 }
