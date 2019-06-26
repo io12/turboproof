7fcfef8028d615ed87c37351fa636ca2c9e990de
Benjamin Levy <blevy@protonmail.com>
2019-06-26 19:59:47 -0400

    add conj and disj functions

--- examples/test.lisp
+++ examples/test.lisp
@@ -2,6 +2,16 @@
 	     (lambda B Prop
 	       (forall x A B))))
 
+(Define conj (lambda A Prop
+	     (lambda B Prop
+	       (forall C Prop
+		       (=> (=> A (=> B C)) C)))))
+
+(Define disj (lambda A Prop
+	     (lambda B Prop
+	       (forall C Prop
+		       (=> (=> A C) (=> (=> B C) C))))))
+
 (Define ~ (lambda A Prop
 	    (forall C Prop (=> A C))))
 
